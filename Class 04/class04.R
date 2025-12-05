7003e45515635f8f2fd4a656eda18824a1563080
Qingyun Zheng <qizheng@ucsd.edu>
2025-12-05 14:44:00 -0800

    class4, 11, 12

---
+++ Class 04/class04.R
@@ -0,0 +1,16 @@
+#' ---
+#' title: "BGGN213 Lab04"
+#' author: "Qingyun Zheng"
+#' ---
+
+
+
+#This is my first R script
+x <- 1:50
+plot(x)
+plot(x,type = "b", color = "green")
+plot(x, sin(x), col = "red", type = "l", lwd = 4)
+log(10)
+log(exp(1))
+log(10, base = 10)
+plot(x,log(x, base = 10), type = "l")
